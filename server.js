00f50457edd25994811a09fdf880a4f0cea2d56c
jaiacouc <jaiacouc@uncg.edu>
2022-03-22 10:17:01 -0400

    added dotenv for github

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const morgan = require("morgan");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+const { DB_PASS } = process.env;
 
 // Define express app
 const app = express();
@@ -14,7 +18,7 @@ const PORT = process.env.PORT || 4000;
 // Connect to database
 mongoose
   .connect(
-    "mongodb://lofiapi:ZYetGIyJCXCfcVBQEAv3HW6r8I5kfINogKOSukq682V4dIybvFbGDTc703jhFsE9KVDQLZ2qAXfqQy4LVobvlQ==@lofiapi.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@lofiapi@",
+    `mongodb://lofiapi:${DB_PASS}@lofiapi.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@lofiapi@`,
     { useNewUrlParser: true }
   )
   .then(() => {
